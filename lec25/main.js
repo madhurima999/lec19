a7bdb63e01b96967af9253baa7a4a25d00915a8c
madhurima999 <gorainmadhurima999@gmail.com>
2023-05-31 23:05:42 +0530

    w

---
+++ lec25/main.js
@@ -0,0 +1,12 @@
+let gp=document.getElementById("grandparent");
+let p=document.getElementById("parent");
+let c=document.getElementById("child");
+
+gp.addEventListener("click",e=>{console.log("capturing gp")},{capture:true});
+p.addEventListener("click",e=>{console.log("capturing p")},{capture:true});
+
+c.addEventListener("click",e=>{console.log("capturing c")},{capture:true});
+
+gp.addEventListener("click",e=>console.log("bubbling"+e.target.textContent));
+p.addEventListener("click",e=>console.log("bubbling"+e.target.textContent));
+c.addEventListener("click",e=>console.log("bubbling"+e.target.textContent));
