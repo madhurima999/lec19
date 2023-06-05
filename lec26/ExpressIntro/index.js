77643fa7aeed6339271ba5e6879bb890357c4a68
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-05 20:21:57 +0530

    w

--- lec26/ExpressIntro/index.js
+++ lec26/ExpressIntro/index.js
@@ -1,45 +1,45 @@
 
-// const express = require("express");
-// const colors = require("colors");
+const express = require("express");
+const colors = require("colors");
 
-// const app = express()
+const app = express()
 
-// // console.log(app)
-
-
-// // app.use((req,res)=>{
-
-// //     console.log(" application running");
-
-// //     res.send(" hello from server");
-
-// // })
-
-// app.get("/",(req,res)=>{
-
-//     res.status(200).send("Home route!")
-// })
+// console.log(app)
 
 
-// app.get("/product",(req,res)=>{
+// app.use((req,res)=>{
 
-//     res.status(200).send("<h1>you are seeing some products</h1>")
-// })
+//     console.log(" application running");
 
-
-// app.get("/details",(req,res)=>{
-
-//     res.status(200).send({ some: 'json' })
-// })
-
-
-// app.all("*", (req,res)=>{
-
-//     res.status(404).send("oops! file not found or wrong path!")
+//     res.send(" hello from server");
 
 // })
 
-// app.listen("8000", (req,res)=>{
+app.get("/",(req,res)=>{
 
-//     console.log("server is running at port 8000".red)
-// })
+    res.status(200).send("Home route!")
+})
+
+
+app.get("/product",(req,res)=>{
+
+    res.status(200).send("<h1>you are seeing some products</h1>")
+})
+
+
+app.get("/details",(req,res)=>{
+
+    res.status(200).send({ some: 'json' })
+})
+
+
+app.all("*", (req,res)=>{
+
+    res.status(404).send("oops! file not found or wrong path!")
+
+})
+
+app.listen("8000", (req,res)=>{
+
+    console.log("server is running at port 8000".red)
+})
