43a5987dc7ab3c8b8cc3853483cf79e1d5b30143
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-05 16:44:22 +0530

    lec26 created

---
+++ lec26/Nodemodules/main.js
@@ -0,0 +1,8 @@
+// const os= require("os");
+// const user=os.userInfo();
+// console.log(user);
+// console.log(os.cpus());
+// console.log(os.totalmem());
+const path=require("path");
+path.join("/main","content","abc.txt");
+ 
