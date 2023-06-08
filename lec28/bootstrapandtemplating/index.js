0a4882bd22be98f15374276132479b31005fe5bf
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-09 00:13:46 +0530

    q

--- lec28/bootstrapandtemplating/index.js
+++ lec28/bootstrapandtemplating/index.js
@@ -7,11 +7,33 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname,"public")));
 
+const todos = [
+
+    "Avatar",
+    "Avengers",
+    "KGF",
+    "Shawshank Redemption",
+    "John Wick"
+]
 
 app.get("/",(req,res)=>{
     let randomNumber=Math.floor(Math.random()*10);
     res.render("index",{randomNumber});
 })
+
+app.get("/random",(req,res)=>{
+
+    const randomNumber = Math.floor(Math.random()*10);
+
+    res.render("random", { favNumber: randomNumber})
+})
+
+app.get("/todos",(req,res)=>{
+
+    res.render("todos", {todos})
+
+})
+
 app.listen(PORT,(req,res)=>{
 console.log(`listening at ${PORT}`);
 })
