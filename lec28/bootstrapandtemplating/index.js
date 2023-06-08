0b8ca59f7ed1d7800d61d3ad7acc395e108b857b
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-08 23:54:19 +0530

    ee

--- lec28/bootstrapandtemplating/index.js
+++ lec28/bootstrapandtemplating/index.js
@@ -4,7 +4,9 @@ const PORT=8000;
 const path=require("path"); 
 
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname, "views"))
+app.set("views",path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname,"public")));
+
 
 app.get("/",(req,res)=>{
     let randomNumber=Math.floor(Math.random()*10);
