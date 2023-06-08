771fa2183f8bc37fa57638f858f90f2c9cf9fe68
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-09 02:07:43 +0530

    p

--- lec28/bootstrapandtemplating/index.js
+++ lec28/bootstrapandtemplating/index.js
@@ -1,11 +1,13 @@
-const express =require("express");
-const app=express();
-const PORT=8000;
-const path=require("path"); 
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const path = require("path")
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname,"public")));
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 const todos = [
 
@@ -16,14 +18,16 @@ const todos = [
     "John Wick"
 ]
 
-app.get("/",(req,res)=>{
-    let randomNumber=Math.floor(Math.random()*10);
-    res.render("index",{randomNumber});
-})
+// get request to view my template
 
+app.get("/",(req,res)=>{
+
+    res.render("index");
+
+})
 app.get("/random",(req,res)=>{
 
-    const randomNumber = Math.floor(Math.random()* 10);
+    const randomNumber = Math.floor(Math.random()*10);
 
     res.render("random", { favNumber: randomNumber})
 })
@@ -33,7 +37,18 @@ app.get("/todos",(req,res)=>{
     res.render("todos", {todos})
 
 })
+app.get("/userdetail", (req,res)=>{
 
-app.listen(PORT,(req,res)=>{
-console.log(`listening at ${PORT}`);
+    console.log(req.query)
+
+    res.send("get request completed");
+
+})
+app.get("/blog",(req,res)=>{
+    res.render("blog");
+})
+app.listen(PORT, (req,res)=>{
+
+    console.log(`server is running at port ${PORT}`)
+
 })
