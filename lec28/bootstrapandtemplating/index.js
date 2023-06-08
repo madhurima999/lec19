cba9f5751c28fa941141b4002bc2f500aab9fed5
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-08 17:14:27 +0530

    ty

---
+++ lec28/bootstrapandtemplating/index.js
@@ -0,0 +1,15 @@
+const express =require("express");
+const app=express();
+const PORT=8000;
+const path=require("path"); 
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname, "views"))
+
+app.get("/",(req,res)=>{
+    let randomNumber=Math.floor(Math.random()*10);
+    res.render("index",{randomNumber});
+})
+app.listen(PORT,(req,res)=>{
+console.log(`listening at ${PORT}`);
+})
