7e94efd3a2693d3988303498e05a379ff37cb8cd
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-10 16:39:27 +0530

    fs

--- nodejscoremodules/index.js
+++ nodejscoremodules/index.js
@@ -1,2 +1,13 @@
-const name="madhurima";
-console.log(name);
+const fs=require('fs');
+//creating new file
+fs.writeFileSync('read.txt',"welcome");
+
+fs.writeFileSync('read.txt',"thijbdciujbdsi");
+//how to add in file
+fs.appendFileSync('read.txt',"  bdajskbadujidb");
+
+
+const data=fs.readFileSync("read.txt");
+console.log(data);
+const t=data.toString();
+console.log(t);
