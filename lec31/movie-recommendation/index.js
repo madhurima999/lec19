af3b1a5767c85beb3acff2aa0610fa102472eb22
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-11 21:43:24 +0530

    i

---
+++ lec31/movie-recommendation/index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const app= express();
+const port=3000;
+const path=require("path");
+const mongoose=require("mongodb://127.0.0.1:27017/movies-recomm").then(()=>{
+    console.log("Connection done");
+}).catch((err)=>{
+console.log(err);
+})
+app.set("view-engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
