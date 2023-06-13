ddec36594ff4375c2f67bcbe3804ec3dad0ee460
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-13 22:49:22 +0530

    cimmit

--- lec34/session-demo/index.js
+++ lec34/session-demo/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const session= require('express-session');
+const flash=require('connect-flash')
 
 const sessionConfig={
     secret: 'keyboard cat',
