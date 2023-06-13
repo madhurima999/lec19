63bca8567680bcb06bfc23ad056c871b30427951
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-13 22:28:56 +0530

    p

--- lec34/session-demo/index.js
+++ lec34/session-demo/index.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const session= require('express-session');
 
-app.use(express.json())
+const sessionConfig={
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+   
+}
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.json({ success: true })
+app.use(session(sessionConfig))
+
+app.get('/', (req, res)=>{
+res.send("home route");
 })
+
+app.get('/setuser', (req,res)=>{
+    const {username}=req.query;
+    req.session.username= username;
+res.send("send data ");
+})
+
+app.get('/salutation', (req,res)=>{
+    const {username}=req.session;
+    console.log(req.session);
+    res.send(`hello, good ${username}`);
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
