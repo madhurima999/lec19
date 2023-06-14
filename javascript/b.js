9e1df3f60d1286536124bf53e5de8c305914b9e5
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-14 16:56:22 +0530

    p

---
+++ javascript/b.js
@@ -0,0 +1,7 @@
+let ctitle=document.getElementsByClassName("card-title")[2];
+
+ctitle.style.color="red";
+
+let  ctitles=document.querySelectorAll(".card-title");
+console.log(ctitles);
+ctitles[0].style.color="pink";
---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+let t=document.body.firstElementChild;
+// console.log(t);
+// console.log(t.tHead.firstElementChild.firstElementChild.firstChild)
+// console.log(t.tHead.firstElementChild.firstElementChild.firstElementChild);
+// //tbodies is collection so use index system
+// console.log(t.tBodies[0].firstElementChild.firstChild)
+
+console.log(t.tBodies[0].rows[0].cells)
