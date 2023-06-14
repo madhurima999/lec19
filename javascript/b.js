b14663f420cf30d23d33281f1f972f9cfd04d3a1
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-14 19:16:36 +0530

    l

--- javascript/b.js
+++ javascript/b.js
@@ -5,3 +5,9 @@ ctitle.style.color="red";
 let  ctitles=document.querySelectorAll(".card-title");
 console.log(ctitles);
 ctitles[0].style.color="pink";
+
+document.querySelector(".this").style.backgroundColor="black";
+
+console.log(document.querySelector(".card").getElementsByTagName('a'))
+
+console.log(document.getElementsByName("hello"))
