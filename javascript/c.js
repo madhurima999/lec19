1b466b832da855261ab1c0c2f111f6f1d764456f
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-15 17:22:11 +0530

    38

---
+++ javascript/c.js
@@ -0,0 +1,6 @@
+let id1=document.getElementById("id1");
+let id3=document.getElementById("id3");
+console.log(id1);
+// console.log(id1.matches(".box"))
+// console.log(id3.closest("#id3"))
+console.log(id1.contains(id3))
---
+++ javascript/d.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("nav")[0].firstElementChild.style.color="red";
+
+document.getElementsByTagName("nav")[0].firstElementChild.style.color="red";
+document.getElementsByTagName("nav")[0].lastElementChild.style.color="red";
+
+
+Array.from(document.getElementsByTagName("li")).forEach((ele)=>{
+    ele.style.background="pink"
+})
