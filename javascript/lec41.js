4b6593fe2b87b64b4cfa426ea6f45cb5643db2dd
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-16 00:40:50 +0530

    j

---
+++ javascript/lec41.js
@@ -0,0 +1,7 @@
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName)
+console.log(document.getElementsByTagName('span')[0])
+let f=document.getElementsByTagName('span')[0]
+console.log(f.innerHTML);
+console.log(f.outerHTML);
+console.log(f.textContent)
