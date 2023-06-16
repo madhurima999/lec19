78f446e9c20288971432e58e114896fe7c4f2d62
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-17 01:35:22 +0530

    i

--- javascript/lec41.js
+++ javascript/lec41.js
@@ -1,7 +1,30 @@
-console.log(document.body.firstChild.nodeName);
-console.log(document.body.firstElementChild.nodeName)
-console.log(document.getElementsByTagName('span')[0])
-let f=document.getElementsByTagName('span')[0]
-console.log(f.innerHTML);
-console.log(f.outerHTML);
-console.log(f.textContent)
+// console.log(document.body.firstChild.nodeName);
+// console.log(document.body.firstElementChild.nodeName)
+// console.log(document.getElementsByTagName('span')[0])
+// let f=document.getElementsByTagName('span')[0]
+// console.log(f.innerHTML);
+// console.log(f.outerHTML);
+// console.log(f.textContent)
+
+// let a=first.getAttribute('class');
+// console.log(a);
+
+// first.setAttribute('class' ,'g');
+// let b=first.getAttribute('class');
+// console.log(b);
+
+let a=document.getElementsByTagName('section')[0]
+a.innerHTML=a.innerHTML+'<h1>Hello</h1>';
+
+let div=document.createElement('div');
+div.className='alert';
+div.innerHTML='<span>Hello</span>'
+document.body.append(div);
+
+for(let i=0;i<9;i++){
+   
+let div=document.createElement('div');
+div.className='alert'+i;
+div.innerHTML='<span>Hello</span>'+i
+document.body.append(div); 
+}
