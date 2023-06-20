33d589bb364c9bfde103214ee138b3f3d5cc8765
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-21 02:50:48 +0530

    w

--- javascript/class.js
+++ javascript/class.js
@@ -1,2 +1,26 @@
-first.className='text-dark red';
-first.classList.remove("red");  
+// first.className='text-dark red';
+// first.classList.remove("red");  
+
+
+// let a=prompt("enter good number");
+
+// let x=function(e){
+   
+//     alert("hello world")
+// }
+let y=function(e){
+    console.log(e);
+    console.log(e.target);
+    alert("hello world2")
+}
+
+btn.addEventListener('click',y)
+
+// btn.addEventListener('click',y)
+
+
+// if(a==='2'){
+//     btn.removeEventListener('click',y)
+// }
+
+
