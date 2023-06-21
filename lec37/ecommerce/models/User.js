386681a39362f6474c43ad0393ca347e7ac43390
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-21 21:14:53 +0530

    p

--- lec37/ecommerce/models/User.js
+++ lec37/ecommerce/models/User.js
@@ -8,7 +8,25 @@ const userSchema = new mongoose.Schema({
     // username : String, ---> This is automatically added by passport-local-mongoose, no need to add ourselves
     // password : String,  ---> This is automatically added by passport-local-mongoose
 
-    email : String
+    email : String,
+
+    cart: [
+        {
+            name: String,
+            price: Number,
+            img: String,
+            id: mongoose.Schema.Types.ObjectId,
+            count : {
+                type:Number,
+                default: 1,
+                min: [0, "Quantity cannot ne less than 1"]
+            }
+
+        }
+
+
+
+    ]
 })
 
 
