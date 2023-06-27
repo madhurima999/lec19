34422aeddf85d129fbf8636b80d1f9247a930d23
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-27 17:11:02 +0530

    r

--- javascript/callback.js
+++ javascript/callback.js
@@ -7,3 +7,23 @@
 setTimeout(function(){
     console.log("hey")
 },5000)
+
+
+function loadScript(src, callback){
+    var script=document.createElement("script");
+    script.src=src;
+    let r="i";
+    script.onload=function(){
+        console.log("loaded")
+       callback(src);
+    }
+    document.body.appendChild(script);
+}
+function hello(src){
+    alert("hello"+src)
+}
+function goodmorning(){
+    console.log("goodmorning")
+}
+
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js",hello)
