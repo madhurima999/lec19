e69b6d0a199a988642f9c844288110de70378e3b
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-27 16:49:56 +0530

    e

--- javascript/callback.js
+++ javascript/callback.js
@@ -1,4 +1,9 @@
 
-const prompt=require("prompt-sync")({sigint:true});
-let a= prompt("what is your name");
-console.log(a) 
+// const prompt=require("prompt-sync")({sigint:true});
+// let a= prompt("what is your name");
+// console.log(a) 
+
+//asynchronous
+setTimeout(function(){
+    console.log("hey")
+},5000)
