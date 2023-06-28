4cf83e5dd3d48c052ffaf997bf10c0fcfbc466c4
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-28 14:29:04 +0530

    e

--- javascript/54.js
+++ javascript/54.js
@@ -11,3 +11,40 @@ setTimeout(function(){
 console.log("kon")
 
 console.log(promise)
+
+
+let p1=new Promise((resolve,reject)=>{
+    console.log("promise is pending")
+    setTimeout(()=>{
+        console.log("I am a promise and i am fulfilled")
+      resolve(true)
+     
+    },5000)
+  
+})
+
+let p2=new Promise((resolve,reject)=>{
+    console.log("promise is pending")
+    setTimeout(()=>{
+        console.log("I am a promise and i am fulfilled")
+       // resolve(true)
+       reject(new Error("i am error"))
+    },5000)
+  
+})
+//this takes resolve value
+p1.then((value)=>{
+    console.log(value);
+})
+//this takes reject value
+p2.catch((error)=>{
+    console.log(error)
+})
+
+p2.then((value)=>{
+    console.log("kk")
+},(error)=>{
+console.log(error)
+})
+
+// console.log(p1,p2)
