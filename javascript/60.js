b46df31f46687b51f726cd70bab2df8314819d9b
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-29 15:17:32 +0530

    i

---
+++ javascript/60.js
@@ -0,0 +1,14 @@
+setTimeout(()=>{
+    console.log("ss")
+},1000)
+
+try{
+    
+    console.log(rahul)
+}catch(error){
+    console.log(error)
+}
+
+setTimeout(()=>{
+    console.log("ok")
+},2000)
