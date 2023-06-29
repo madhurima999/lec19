a495318cf98ac239c28ca71d6b49e76b0f5890fd
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-29 18:26:25 +0530

    w

--- javascript/60.js
+++ javascript/60.js
@@ -12,3 +12,21 @@
 // setTimeout(()=>{
 //     console.log("ok")
 // },2000)
+
+const loadScript=async(src)=>{
+    return new Promise((resolve,reject)=>{
+        let script=document.createElement("script")
+        script.src=src
+        script.onload=()=>{
+            resolve(src)
+        }
+        document.head.append(script)
+    })
+
+}
+const main1=async()=>{
+    let a= await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
+    console.log(a)
+}
+
+main1()
