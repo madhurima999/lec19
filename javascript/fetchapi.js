4a535d835fb08ae9ab915d869f74a1862b5ba490
madhurima999 <gorainmadhurima999@gmail.com>
2023-07-09 20:38:53 +0530

    s

---
+++ javascript/fetchapi.js
@@ -0,0 +1,13 @@
+// let p=fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London%2CUK?unitGroup=us&key=8LZFPHJXLVVCCBCCSKLGXBM3B")
+
+// p.then((value1)=>{
+// return value1
+// }).then((value2)=>{
+//     console.log(value2)
+// })
+
+let options={
+    method:"POST",
+    headers:{"Content-type":"application/json"
+     }
+}
