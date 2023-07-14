337c232a12dfc0ef2c8078bbdc8a2e53c6a328fd
madhurima999 <gorainmadhurima999@gmail.com>
2023-07-14 23:41:37 +0530

    g

--- javascript/abc.js
+++ javascript/abc.js
@@ -4,3 +4,16 @@
 //     },2000)
 // })  
 
+function play()
+{
+    var audio=new Audio('Tu-Hai-To-Mujhe-Phir-Aur-Kya-Chahiye(PaglaSongs).mp3');
+    audio.play();
+}
+setInterval(()=>{
+    let d=new Date()
+    // document.write(d.getTime())
+time.innerHTML=d.toDateString()+" "+d.toTimeString()
+},100)
+// setTimeout(()=>{
+//     play()
+// },2000);
